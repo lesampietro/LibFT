3b8cc8ec88c65ca614e8f3549a89d527ea3b4544
Letícia Sampietro <lsampiet@student.42sp.org.br>
2023-10-20 05:26:22 -0300

    ft_striteri v02

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 05:10:39 by lsampiet          #+#    #+#             */
-/*   Updated: 2023/10/20 05:24:12 by lsampiet         ###   ########.fr       */
+/*   Updated: 2023/10/20 05:25:40 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		i++;
 	}
 }
-
-//Example: BANANA
-//The letter "B" in "BANANA" is equal to s[0] or
-//	to s[i] (before incrementing).
-//In this function, we want the string's character 
-//	adress to be passed as argument.
-//That's why we use "&s[i]".
